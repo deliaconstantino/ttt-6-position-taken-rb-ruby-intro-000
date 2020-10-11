1bf5e2d717afe98784bdd8f14a706b6d66eda685
Delia Constantino <dconstan92@gmail.com>
2020-10-11 22:40:43 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,19 @@
+# def position_taken?(board, index)
+#   if board[index] == "X" || board[index] == "O"
+#     return true
+#   elsif board[index] == " "
+#     return false
+#   elsif board[index] == ""
+#     return false
+#   elsif board[index] == nil
+#     return false
+#   end
+# end
+
 def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
     return true
-  elsif board[index] == " "
-    return false
-  elsif board[index] == ""
-    return false
-  elsif board[index] == nil
+  elsif
     return false
   end
 end
